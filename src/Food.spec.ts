9e53193ceb4831383568c2e57204f0b78810c927
Nitai Charan <nitaicharan@gmail.com>
2022-03-06 02:59:19 -0500

    9. Add name and unit to Food

--- src/Food.spec.ts
+++ src/Food.spec.ts
@@ -2,8 +2,10 @@ import Food from './Food'
 
 describe('', () => {
     test('create', () => {
-        const food = new Food();
+        const food = new Food('rice', 'g');
 
         expect(food).toBeDefined();
+        expect(food.getName()).toEqual('rice');
+        expect(food.getUnit()).toEqual('g');
     });
 });
--- src/Food.ts
+++ src/Food.ts
@@ -1,5 +1,16 @@
 class Food {
+    constructor(
+        private readonly name: string,
+        private readonly unit: string,
+    ) { }
 
+    getUnit(): any {
+        return this.unit;
+    }
+
+    getName() {
+        return this.name;
+    }
 }
 
 export default Food;
