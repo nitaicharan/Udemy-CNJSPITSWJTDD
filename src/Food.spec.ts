179bc619f8bb3d769bae9fce16ac4d343d9d4d58
Nitai Charan <nitaicharan@gmail.com>
2022-03-06 03:18:39 -0500

    10. Add base values to Food

--- src/Food.spec.ts
+++ src/Food.spec.ts
@@ -2,10 +2,20 @@ import Food from './Food'
 
 describe('', () => {
     test('create', () => {
-        const food = new Food('rice', 'g');
+        const food = new Food('rice', 'g', {
+            amount: 100,
+            fat: 30,
+            carbohydrate: 40,
+            protein: 65,
+            calories: 124,
+        });
 
         expect(food).toBeDefined();
         expect(food.getName()).toEqual('rice');
         expect(food.getUnit()).toEqual('g');
+        expect(food.getBaseValues().amount).toEqual(30);
+        expect(food.getBaseValues().carbohydrate).toEqual(40);
+        expect(food.getBaseValues().protein).toEqual(65);
+        expect(food.getBaseValues().calories).toEqual(124);
     });
 });
--- src/Food.ts
+++ src/Food.ts
@@ -1,8 +1,15 @@
+import Nutritions from "./Notritions";
+
 class Food {
     constructor(
         private readonly name: string,
         private readonly unit: string,
+        private readonly baseAmount: Nutritions,
     ) { }
+
+    getBaseValues() {
+        return this.baseAmount;
+    }
 
     getUnit(): any {
         return this.unit;
